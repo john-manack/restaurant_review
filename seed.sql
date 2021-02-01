786981e442396586c75d5348eb9cfe6729263d21
John Manack <jmnck28@gmail.com>
2021-02-01 16:17:33 -0500

    adds seed data

---
+++ seed.sql
@@ -0,0 +1,6 @@
+INSERT INTO restaurants (name, distance, stars, category, favorite_dish, does_takeout, last_dining_date) VALUES
+('Bone Garden', 3, 3.5, 'Mexican', 'Chicken Encheladas', TRUE, '2020-11-01 18:30:00'),
+('Das BBQ', 1.5, 3, 'Barbeque', 'Brisket Sandwich', TRUE, '2021-01-03 13:05:00'),
+('Sugar Magnolia', 138, 4, 'Southern Cooking', FALSE, '2020-12-28 12:07:40'),
+('The Optimist', 4, 5, 'Seafood', FALSE, '2020-07-16 19:48:29'),
+('Trapeze', 4.1, 72, 'Pub', FALSE, '2020-08-23 13:06:55');
